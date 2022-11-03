03801b1a6dcf19805867ff3a1ac39dad1515fa6f
莫忧离 <11857381+moyouliv@user.noreply.gitee.com>
2022-11-03 06:57:59 +0000

    !14 storage 合并 * storage xml commit * Merge branch 'master' of gitee.com:blusque/ruoyi_mypc * Merge gitee.com:blusque/ruoyi_mypc * Merge branch 'master' of gitee.com:blusque/ruoyi_mypc * 2022/11/2 commit

--- ruoyi-ui/src/views/storageoutindetail/outindetail/index.vue
+++ ruoyi-ui/src/views/storageoutindetail/outindetail/index.vue
@@ -34,16 +34,6 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="handleAdd"
-          v-hasPermi="['storageoutindetail:outindetail:add']"
-        >新增</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
           type="warning"
           plain
           icon="el-icon-download"
